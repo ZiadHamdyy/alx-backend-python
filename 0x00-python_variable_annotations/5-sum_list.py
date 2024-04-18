08a3369c4b92eb8d2ed3b3cb546ac14a913033e4
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-04-18 19:28:39 +0200

    handle task 6 and 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,7 +6,4 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """type-annotated function sum_list which takes a list input_list"""
-    if not input_list:
-        return 0
-    else:
-        return input_list[0] + sum_list(input_list[1:])
+    return sum(input_list)
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,8 +5,5 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """type-annotated function sum_list which takes a list mxd_lst"""
-    if not mxd_lst:
-        return 0
-    else:
-        return mxd_lst[0] + sum_mixed_list(mxd_lst[1:])
+    """type-annotated function sum_mixed_list which takes a list mxd_lst"""
+    return sum(mxd_lst)
