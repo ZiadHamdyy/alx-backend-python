b40d9ba4fe35edf9b496a962f987de4cf00dec50
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-04-18 20:16:16 +0200

    task 7, 8, 9, 10

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,6 +3,7 @@
 
 from typing import Sequence, Any, Union
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Augment code with the correct duck-typed annotations"""
     if lst:
