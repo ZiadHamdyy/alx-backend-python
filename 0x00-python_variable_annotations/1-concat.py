7fabd4d9fe35b8e9c647e8c1d7acad3e2cb953ae
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-04-18 18:42:47 +0200

    handle documentation

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
 def concat(str1: str, str2: str) -> str:
+    """type-annotated function concat that takes a string
+    str1 and a string str2"""
     return "{}{}".format(str1, str2)
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
 def floor(n: float) -> int:
+    """type-annotated function floor which takes a float n"""
     return int(n)
