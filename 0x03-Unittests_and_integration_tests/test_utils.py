30b62276fb876914221b1d7892c851f083953f56
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-10 16:56:39 +0300

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+"""TestAccessNestedMap"""
+
 import unittest
-from parameterized import parameterized # type: ignore
+from parameterized import parameterized  # type: ignore
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
+    """TestAccessNestedMap class that inherits from unittest.TestCase"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -11,7 +15,22 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
+        """test_access_nested_map method to test that the method returns what
+        it is supposed to"""
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_exception):
+        """test_access_nested_map_exception. Use the assertRaises
+        context manager to test that a KeyError"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), expected_exception)
+
 
 if __name__ == "__main__":
     unittest.main()
