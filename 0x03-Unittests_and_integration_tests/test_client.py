959a4cef59111fe35d10c1b9c96c20b8d1e3af66
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-10 21:19:53 +0300

    task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -32,6 +32,29 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(client._public_repos_url,
                          "https://api.github.com/orgs/testorg/repos")
 
+    @unittest.mock.patch('client.get_json')
+    @unittest.mock.patch('client.GithubOrgClient._public_repos_url',
+                         new_callable=unittest.mock.PropertyMock)
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        """test_public_repos"""
+        sample_payload = [
+            {"name": "repo1", "license": {"key": "MIT"}},
+        ]
+
+        mock_public_repos_url.return_value = \
+            "https://api.github.com/orgs/testorg/repos"
+        mock_get_json.return_value = sample_payload
+
+        client = GithubOrgClient("testorg")
+
+        repos = client.public_repos(license="MIT")
+
+        mock_get_json.assert_called_once_with(
+            "https://api.github.com/orgs/testorg/repos")
+        mock_public_repos_url.assert_called_once()
+
+        self.assertEqual(repos, ["repo1"])
+
 
 if __name__ == "__main__":
     unittest.main()
